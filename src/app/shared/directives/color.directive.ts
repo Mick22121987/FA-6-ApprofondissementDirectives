46b024219f3e1b2096eb6849ac9dbb9cca8425b0
Mick22121987 <m.vanaelst@wavenet.be>
2018-10-25 11:52:06 +0200

    L4 HostBinding

--- src/app/shared/directives/color.directive.ts
+++ src/app/shared/directives/color.directive.ts
@@ -1,18 +1,20 @@
-import { Directive, ElementRef, OnInit, Renderer2, HostListener } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appColor]'
 })
 export class ColorDirective implements OnInit {
-
+  @HostBinding('style.backgroundColor') backgroundColor = 'transparent';
+  @HostBinding('style.color') color = 'blue';
   constructor(private el: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
     // this.el.nativeElement.style.color = 'red';
-    this.renderer.setStyle(this.el.nativeElement, 'color', 'red');
+    // this.renderer.setStyle(this.el.nativeElement, 'color', 'red');
   }
 
   @HostListener('click') click(event: Event) {
-    this.renderer.setStyle(this.el.nativeElement, 'background-color', 'black');
+    // this.renderer.setStyle(this.el.nativeElement, 'background-color', 'black');
+    this.backgroundColor = 'yellow';
   }
 }
