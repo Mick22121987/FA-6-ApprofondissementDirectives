83aa39e4c19579073928ba2f7fe7fa77b9d78b2e
Mick22121987 <m.vanaelst@wavenet.be>
2018-10-25 12:10:16 +0200

    L5 : Liaison propriétés et directive

--- src/app/shared/directives/color.directive.ts
+++ src/app/shared/directives/color.directive.ts
@@ -1,20 +1,24 @@
-import { Directive, ElementRef, OnInit, Renderer2, HostListener, HostBinding } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appColor]'
 })
 export class ColorDirective implements OnInit {
   @HostBinding('style.backgroundColor') backgroundColor = 'transparent';
-  @HostBinding('style.color') color = 'blue';
+  @HostBinding('style.color') color;
+  // tslint:disable-next-line:no-input-rename
+  @Input('appColor') data;
+
   constructor(private el: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
     // this.el.nativeElement.style.color = 'red';
     // this.renderer.setStyle(this.el.nativeElement, 'color', 'red');
+    this.color = this.data.text;
   }
 
   @HostListener('click') click(event: Event) {
     // this.renderer.setStyle(this.el.nativeElement, 'background-color', 'black');
-    this.backgroundColor = 'yellow';
+    this.backgroundColor = this.data.background;
   }
 }
