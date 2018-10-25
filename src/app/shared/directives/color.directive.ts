a0f8f9bafc505a74e0876b688786217390cb5051
Mick22121987 <m.vanaelst@wavenet.be>
2018-10-25 11:46:46 +0200

    L3 HostListener

--- src/app/shared/directives/color.directive.ts
+++ src/app/shared/directives/color.directive.ts
@@ -1,14 +1,18 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appColor]'
 })
 export class ColorDirective implements OnInit {
 
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
     // this.el.nativeElement.style.color = 'red';
     this.renderer.setStyle(this.el.nativeElement, 'color', 'red');
+  }
+
+  @HostListener('click') click(event: Event) {
+    this.renderer.setStyle(this.el.nativeElement, 'background-color', 'black');
   }
 }
